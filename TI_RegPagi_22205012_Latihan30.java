dbd5e8afae35742ddcf06972f80902a2ff9f9635
Bag4s <149363597+Bag4s@users.noreply.github.com>
2023-10-30 15:08:08 +0700

    commit

---
+++ TI_RegPagi_22205012_Latihan30.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hello.bagas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ACER
+ */
+public class TI_RegPagi_22205012_Latihan30 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("\u001b[31mKamu \u001b[32mNgerjain Sendiri \u001b[33mLatihan \u001b[34m17 Sampai 30 \u001b[36mIni ? ");
+        System.out.print("\u001b[33mJawab \u001b[32m(Yoi/Engga ) : ");
+        String Jawab = input.nextLine();
+        if (Jawab.equalsIgnoreCase("yoi")){
+            System.out.println("");
+            System.out.println("\u001b[33mCakep Bener, \u001b[32mGod Job");
+        }else {
+            System.out.println("\u001b[33mTetep \u001b[32mCakep Sih");
+        }
+    }
+    
+}
